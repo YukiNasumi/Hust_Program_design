21cd01d0773632c646f02fb4718c59d3ecdb76c9
YukiNasumi <ireneenriquez657@gmail.com>
2024-09-10 00:31:01 +0800

    三种方案的测试

---
+++ run.sh
@@ -0,0 +1,10 @@
+cd mypro
+./run /root/C-/mypro/request/4（unsatisfied）.cnf 1
+./run /root/C-/mypro/request/5.cnf 1
+./run /root/C-/mypro/request/6.cnf 1
+./run /root/C-/mypro/request/7（unsatisfied）.cnf 1
+./run /root/C-/mypro/request/8（unsatisfied）.cnf 1
+./run /root/C-/mypro/request/9（unsatisfied）.cnf 1
+./run /root/C-/mypro/request/10.cnf 1
+./run /root/C-/mypro/request/11（unsatisfied）.cnf 1
+./run /root/C-/mypro/request/12.cnf 1
---
+++ run_.sh
@@ -0,0 +1,20 @@
+cd mypro
+./run /root/C-/mypro/request2/4（unsatisfied）.cnf 2
+./run /root/C-/mypro/request2/5.cnf 2
+./run /root/C-/mypro/request2/6.cnf 2
+./run /root/C-/mypro/request2/7（unsatisfied）.cnf 2
+./run /root/C-/mypro/request2/8（unsatisfied）.cnf 2
+./run /root/C-/mypro/request2/9（unsatisfied）.cnf 2
+./run /root/C-/mypro/request2/10.cnf 2
+./run /root/C-/mypro/request2/11（unsatisfied）.cnf 2
+./run /root/C-/mypro/request2/12.cnf 2
+
+./run /root/C-/mypro/request3/4（unsatisfied）.cnf 3
+./run /root/C-/mypro/request3/5.cnf 3
+./run /root/C-/mypro/request3/6.cnf 3
+./run /root/C-/mypro/request3/7（unsatisfied）.cnf 3
+./run /root/C-/mypro/request3/8（unsatisfied）.cnf 3
+./run /root/C-/mypro/request3/9（unsatisfied）.cnf 3
+./run /root/C-/mypro/request3/10.cnf 3
+./run /root/C-/mypro/request3/11（unsatisfied）.cnf 3
+./run /root/C-/mypro/request3/12.cnf 3
